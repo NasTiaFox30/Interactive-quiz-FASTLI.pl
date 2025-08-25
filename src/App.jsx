1ab4176e331eb53aeeeb12f5d603fc05613b7704
Nastia Bzova <bzovanastia@gmail.com>
2025-08-25 19:17:30 +0200

    Export default App()

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
 import { useState } from 'react';
+
+function App() {
+    return (
+        <>
+            <header id="header">
+                <div class="logo"><img src="./logo-vector.svg" alt=""/><b> - Znajdź dla siebie fachowca </b></div>
+            </header>
+
+            <div className='quiz-container'> </div>    
+        </>    
+    )
+}
+
+export default App;
