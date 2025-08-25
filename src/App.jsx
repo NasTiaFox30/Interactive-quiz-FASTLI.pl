a54c382460d6f1229e2c265b0c4b472f6927e7ba
Nastia Bzova <bzovanastia@gmail.com>
2025-08-25 21:29:44 +0200

    added .card contaienr (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
     return (
         <>
             <Header />
-            <div className='quiz-container'> </div>    
+            <div className='quiz-container'>
+                <div className='card'></div>
+            </div>    
         </>    
     )
 }
