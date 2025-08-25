2d5a774a2f7cd93fec311e853a075dcf742b2cb9
Nastia Bzova <bzovanastia@gmail.com>
2025-08-25 19:52:39 +0200

    used <Header/>

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header";
 function App() {
     return (
         <>
-            <header>
-                <div className="logo"><img src="./assets/logo-vector.svg"/><b> - Znajdź dla siebie fachowca </b></div>
-            </header>
-
+            <Header />
             <div className='quiz-container'> </div>    
         </>    
     )
