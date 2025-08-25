c4b38713f8aa35f83696ecd22722c40646b49259
Nastia Bzova <bzovanastia@gmail.com>
2025-08-25 19:46:55 +0200

    created component - <Header/>

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+<header>
+    <div className="logo"><img src="./assets/logo-vector.svg" /><b> - Znajdź dla siebie fachowca </b></div>
+        </header>
+    );
+}
+
+export default Header;
