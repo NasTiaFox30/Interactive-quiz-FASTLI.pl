b3d5f5e235c9275df4ba3da96c4f17fc0f6cecfe
Nastia Bzova <bzovanastia@gmail.com>
2025-08-27 18:41:14 +0200

    add prop - startQuiz

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,10 +1,10 @@
 
-export default function StartScreen() {
+export default function StartScreen( startQuiz) {
   return (
     <div className="card">
       <h1>Quiz - Znajdź fachowca dla siebie</h1>
       <p>Odpowiedz na kilka pytań - a my pomożemy dopasować ofertę!</p>
-      <button className="start">Zaczynamy</button>
+      <button className="start" onClick={startQuiz}>Zaczynamy</button>
       </div>
     )
 }
