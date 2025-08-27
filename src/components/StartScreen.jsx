a24ca7784445abcc44aef298b17596d0462a12ec
Nastia Bzova <bzovanastia@gmail.com>
2025-08-27 16:32:23 +0200

    added description start screen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,6 +3,8 @@ export default function StartScreen() {
   return (
     <div className="card">
       <h1>Quiz - Znajdź fachowca dla siebie</h1>
+      <p>Odpowiedz na kilka pytań - a my pomożemy dopasować ofertę!</p>
+      
       </div>
     )
 }
