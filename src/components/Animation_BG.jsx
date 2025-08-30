b74dd801b2ce02b6b415a1978507ed74a37c1947
Nastia Bzova <bzovanastia@gmail.com>
2025-08-30 17:31:46 +0200

    created component - Animation_bg.jsx

---
+++ src/components/Animation_BG.jsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+
+const icons = ['🛠️', '⚙️', '💅', '💇‍♀️', '🫂', '🌳', '🧑‍🌾',  '💡', '🍽️', '🏡', '📖', '💼', '⚡', '🎼', '🧹', '💉', '💻', '📱'];
+
+export default function BG_animation() {
+  return (
+    <div className="bg-animation">
+      {icons.map((icon, idx) => (
+          <motion.span
+            className="floating-icons"
+            key={idx}
+            initial={{ y: "100vh", x: Math.random() * window.innerWidth }}
+            animate={{ y: "-10vh" }}
+            transition={{
+                duration: Math.random() * 5 + 5,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "linear"
+            }}
+        >
+          {icon}
+        </motion.span>
+      ))}
+    </div>
+  );
+}
