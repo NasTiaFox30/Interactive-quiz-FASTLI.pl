fe715f7866673282060dbd0ce7c7803467e2e06d
Nastia Bzova <bzovanastia@gmail.com>
2025-08-30 12:38:37 +0200

    call component ProgressBar

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -3,6 +3,7 @@ import ProgressBar from "./ProgressBar";
 export default function QuestionScreen({ question, onAnswer, step, total }) {
   return (
     <div className="card">
+      <ProgressBar step={step-1} total={total} />
       <h2 className="question">{question.text}</h2>
       <div className="options">
         {question.options.map((option, idx) => (
