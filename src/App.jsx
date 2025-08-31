a277bfa603a3e2319a75804db536d7827d7cc943
Nastia Bzova <bzovanastia@gmail.com>
2025-08-31 16:59:46 +0200

    update anim for ResultScreen

--- src/App.jsx
+++ src/App.jsx
@@ -88,8 +88,8 @@ export default function App() {
                             key="result"
                             initial={{ opacity: 0, scale: 0.95 }}
                             animate={{ opacity: 1, scale: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.5, ease: "easeOut" }}
+                            exit={{ opacity: 0, scale: 0.95 }}
+                            transition={{ duration: 0.5, ease: "easeOut"}}
                         >
                             <ResultScreen resultCategory={resultCategory} resetQuiz={handleRestart} />
                         </motion.div>
